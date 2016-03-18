798bebdaea809a1474dbfb3e95c3c282ef13cc96
Neil Okamoto <neil.okamoto@gmail.com>
2016-03-17 21:38:53 -0700

    bump to mesos 0.28.0 and marathon 0.15.3 also separate environment file from "go.sh"

--- go.sh
+++ go.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export MARATHON_VERSION=v0.13.0
-export MESOS_VERSION=0.26.0-0.2.145.ubuntu1404
-#export MESOS_VERSION=0.22.1-1.0.ubuntu1404
+. mesos-env
 
 docker-compose up -d mesosmaster mesosslave zookeeper
 docker-compose logs
+
