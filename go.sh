f5f52bb64ab5e1a1d89554733b9aefd5294f78df
Neil Okamoto <neil.okamoto@gmail.com>
2016-05-08 09:10:34 -0700

    refactor such that we can run HA or non-HA mode.

--- go.sh
+++ go.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+OPTIND=1
+MESOS_HA="_nonha"
+MARATHON=0
+
+function show_help {
+	printf "$0 [OPTIONS]\n\n"
+	printf "Launches Mesos master and slave in docker\n\n"
+	printf "Options:\n"
+	printf "  -h|-?   show this help\n"
+	printf "  -m      launch marathon also\n"
+	printf "  -z      launch mesos in ha mode with zookeeper\n"
+}
+
+while getopts "h?zm" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    m) MARATHON=1
+	    ;;
+    z) MESOS_HA="_ha"
+        ;;
+    esac
+done
+
 . mesos-env
 
-docker-compose up -d mesosmaster mesosslave zookeeper
+SERVICES="mesosslave${MESOS_HA} "
+
+if [ $MARATHON -eq 1 ]; then SERVICES+="marathon${MESOS_HA}"; fi
+
+docker-compose up -d $SERVICES
 docker-compose logs
 
